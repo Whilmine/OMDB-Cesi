f8f5a74b943a442b93721864f3f1bd7eecd40862
Clair5D5l5p55 <claire.delepee@viacesi.fr>
2018-10-09 16:27:48 +0200

    fin de journée bis

--- symfony/src/Controller/FilmController.php
+++ symfony/src/Controller/FilmController.php
@@ -12,11 +12,77 @@ class FilmController extends AbstractController
      */
     public function query()
     {
-        echo "Querry";
-        die;
+        //echo "Querry";
+        //die;
+        $apiKey = "2b12bb84";
+        $query ="monono";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s=tron&apikey=' . $apiKey);
+        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
+
+        $json = json_decode(curl_exec($ch));
+
+
+
+        foreach ($json->Search as $movie){
+            $ID =$movie->imdbID;
+            $newloop = curl_init();
+            curl_setopt($newloop, CURLOPT_URL, 'http://www.omdbapi.com/?i='.$ID.'&apikey=' . $apiKey);
+            curl_setopt($newloop,  CURLOPT_RETURNTRANSFER, true);
+
+        }
+
+        $jsontest = json_decode(curl_exec($newloop));
+        var_dump($jsontest);
+
 
         return $this->render('film/index.html.twig', [
-            'controller_name' => 'FilmController',
+            'controller_name' => "test",
+           'query' => $query,
+           'liste' => $json->Search,
+
         ]);
     }
+
+    /**
+     * @Route("/film_avec_parametres/{query}", name="Film avec paramètre")
+     */
+    public function affichageFilmavecParametres($query)
+    {
+        //echo "Querry";
+        //die;
+        $apiKey = "2b12bb84";
+        //$query ="monono";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s='. $query .'&apikey='. $apiKey);
+        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
+
+        $json = json_decode(curl_exec($ch));
+
+
+
+        foreach ($json->Search as $movie){
+            $ID =$movie->imdbID;
+            $newloop = curl_init();
+            curl_setopt($newloop, CURLOPT_URL, 'http://www.omdbapi.com/?i='.$ID.'&apikey=' . $apiKey);
+            curl_setopt($newloop,  CURLOPT_RETURNTRANSFER, true);
+
+        }
+
+        $jsontest = json_decode(curl_exec($newloop));
+        var_dump($jsontest);
+
+
+        return $this->render('film/index.html.twig', [
+            'controller_name' => "test",
+            'query' => $query,
+            'liste' => $json->Search,
+
+        ]);
+    }
+
+
+
 }
